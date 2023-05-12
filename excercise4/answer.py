f59855cc3d5817fca1b8b5285f1d9176c27987b2
epichyperdevcoder <133328366+epichyperdevcoder@users.noreply.github.com>
2023-05-12 16:12:39 +0800

    Create answer.py

---
+++ excercise4/answer.py
@@ -0,0 +1,23 @@
+"""
+Write a Python program to count the number of vowels in a given text.
+
+Your program should ask the user to enter a text and then print the number of vowels in the text.
+
+Sample input:
+The quick brown fox jumps over the lazy dog.
+
+Sample output:
+11
+"""
+
+text = input("Enter a text: ")
+
+vowels = ["a", "e", "i", "o", "u"
+
+count = 0
+
+for i in text:
+  if i == vowels:
+    count += 1
+
+print(count)
