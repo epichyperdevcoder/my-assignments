7906a72c21e93873411842bac37b0349a0eb3e32
epichyperdevcoder <133328366+epichyperdevcoder@users.noreply.github.com>
2023-05-12 16:14:11 +0800

    Try fixing it more

--- excercise4/answer.py
+++ excercise4/answer.py
@@ -17,7 +17,7 @@ vowels = ["a", "e", "i", "o", "u"]
 count = 0
 
 for i in text:
-  if i == vowels:
+  if i == vowels[0] or vowels[1] or vowels[2] or vowels[3] or vowels[4]:
     count += 1
 
 print(count)
